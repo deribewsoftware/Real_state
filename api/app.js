7764719cca2d2028cf90b9e793c6b40e68e86a09
deribewsoftware <deribewsoftware@gmail.com>
2024-04-04 00:32:12 +0300

    updated

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import postRouter from "./routes/post.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 //api
 app.use("/api/post",postRouter)
 app.use("/api/auth",authRouter)
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,58 @@
-export const register=(req,res)=>{
-  console.log(req.body)
-  res.send("user registration success")
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
+
+export const register=async(req,res)=>{
+ const  {username,email,password} = req.body
+
+try{
+  const hashedPassword=await bcrypt.hash(password,10)
+  const newUser=await prisma.user.create({
+   data:{
+     username:username,
+     password:hashedPassword,
+    email:email
+   }
+  })
+ 
+   res.status(201).json({message:"user registration success", success:true,user:newUser})
+}
+catch(err){
+ 
+  res.status(500).json({message:"user registration error",success:false})
+}
 }
 
 
 
-export const login=(req,res)=>{
-  res.send("user signin success")
+export const login=async(req,res)=>{
+
+  const {password,username} = req.body;
+
+  try{
+    // CHECK USER EXISTS
+    const user=await prisma.user.findUnique({where: {username:username}})
+
+    if(!user){
+return res.status(401).json({message:"invalid credentials"});
+    }
+
+    // COMPARE PASSWORD
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+      return res.status(401).json({message:"invalid credentials"});
+    }
+
+
+
+    // SAVE USER COOKIES AND SEND TO USER
+    // res.setHeader("Set-Cookie","test"+"myValue").json("success")
+    res.cookie("test","myValue",{httpOnly:true,secure:true})
+  }
+  catch(err){
+    res.status(500).json({message:"user login error",success:false})
+  }
+  
 }
 
 
